035ef89f0af435e5053beca732f4f6cef924b2f5
NO NaMe <36953143+Soocyang@users.noreply.github.com>
2020-12-21 22:47:58 +0800

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -3,11 +3,13 @@ const Discord = require('discord.js');
 module.exports = {
     name: 'help',
     description: 'HELP!',
-    execute(msg, client){
+    async execute(msg, client){
+        const devID = '380295136774586369';
+        const user = client.users.cache.find(user => user.id === devID);
         const embed = new Discord.MessageEmbed()
                         .setColor(0x3ba3ee)
                         .setAuthor(client.user.username, client.user.displayAvatarURL())
-                        .setTimestamp()              
+                        .setThumbnail(client.user.displayAvatarURL())
                         .setDescription(
                             'Need help? Here it is.\n\n' + 
                             '**Commands**\n\n' +
@@ -16,7 +18,8 @@ module.exports = {
                             '`~tag edit [name] [description]` to **Edit** tag \n' +
                             '`~tag info [name]` to **Show** tag **info** \n' + 
                             '`~tag remove [name]` to **Remove** tag \n' + 
-                            '`~tag list` to **show all** tags list \n\n');
+                            '`~tag list` to **show all** tags list')
+                        .setFooter(`By ${user.username}`, `${user.displayAvatarURL()}`);
         msg.channel.send(embed);
     }
 };
