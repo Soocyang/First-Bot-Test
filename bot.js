b0b53b2ec00ef87eaf2e60def139f71285f5e589
NO NaMe <36953143+Soocyang@users.noreply.github.com>
2020-12-21 19:10:33 +0800

    Enchaced Tag System

--- bot.js
+++ bot.js
@@ -6,16 +6,6 @@ const client = new Discord.Client({
     partials: ['MESSAGE'],
 });
 
-const PREFIX = '~'
-const ROLE_COLOR = {
-  sky: '787976983714988042',
-  mint: '788222969549684767',
-  grape: '788223025824006164',
-  peach: '788223196545286214',
-  lemon: '788223232373293086',
-  orange: '788223321140363276'
-}
-
 const sequelize = new Sequelize('database', 'user', 'password', {
 	host: 'localhost',
 	dialect: 'sqlite',
@@ -23,7 +13,6 @@ const sequelize = new Sequelize('database', 'user', 'password', {
 	// SQLite only
 	storage: 'database.sqlite',
 });
-
 
 //Create model
 const Tags = sequelize.define('tags', {
@@ -40,6 +29,19 @@ const Tags = sequelize.define('tags', {
 	},
 });
 
+client.once('ready', () => {
+  Tags.sync();
+})
+
+const PREFIX = '~'
+const ROLE_COLOR = {
+  sky: '787976983714988042',
+  mint: '788222969549684767',
+  grape: '788223025824006164',
+  peach: '788223196545286214',
+  lemon: '788223232373293086',
+  orange: '788223321140363276'
+}
 
 const fs = require('fs');
 client.commands = new Discord.Collection();
@@ -50,24 +52,13 @@ for(const file of commandFiles){
   client.commands.set(command.name, command);
 }
 
-client.once('ready', () => {
-  Tags.sync();
-})
+
 
 //Bot activity status
 client.on('ready', () => {
   console.log("Bot is ready!!!");
   client.user.setActivity('Mr & Mrs Gao', { type:'WATCHING' });
 });
-
-//This when user delete message YELL AT THEM!!!!
-// client.on('messageDelete', msg => {
-//     // Create the attachment using MessageAttachment
-//     const attachment = new Discord.MessageAttachment('https://images-ext-2.discordapp.net/external/gSnYcbkGLhtVvwNloUomWx0762Nnfeduwkembi-SBy0/https/media.discordapp.net/attachments/620183192879628298/735538633255419924/unknown.png');
-//     // Send the attachment in the message channel
-//     msg.channel.send(attachment);
-// });
-
 
 //Default message reply memes
 client.on('message', msg => {
@@ -76,119 +67,6 @@ client.on('message', msg => {
 });
 
 //Useful functions command
-client.on('message', msg => {
-  if (!msg.content.startsWith(PREFIX) || msg.author.bot) return;
-  const args = msg.content.slice(PREFIX.length).trim().split(' ');
-  const command = args.shift().toLowerCase();
-  if(command === 'args-info'){
-    checkArgs(msg, args);
-    msg.channel.send(`Command name: ${command}\nArguments: ${args}`);
-  }
-  else if(command === 'clearchat'){
-    checkArgs(msg, args);
-    client.commands.get('clearchat').execute(msg, args);
-    //clearChat(msg, args);
-  }
-  else if(command === 'help'){
-    client.commands.get('help').execute(msg, client);
-  }
-  else if(command === 'admhelp'){
-    client.commands.get('admhelp').execute(msg, client);
-  }
-  else if(command === 'avatar'){
-    checkArgs(msg, args);
-    client.commands.get('avatar').execute(msg, args);
-    //avatar(msg);
-  }
-  else if(command === 'ping'){
-    client.commands.get('ping').execute(msg, args);
-  }
-});
-
-
-client.on('message', async msg => {
-
-  if (!msg.content.startsWith(PREFIX) || msg.author.bot) return;
-  const args = msg.content.slice(PREFIX.length).trim().split(' ');
-  const command = args.shift().toLowerCase();
-
-
-  if (command === 'addtag') {
-    const tagName = args[0];
-    const tagDescription = args[1];
-
-    try {
-      // equivalent to: INSERT INTO tags (name, description, username) values (?, ?, ?);
-      const tag = await Tags.create({
-    		name: tagName,
-    		description: tagDescription,
-    		username: msg.author.username,
-    	});
-    	return msg.reply(`Tag ${tag.name} added.`);
-    }
-    catch (e) {
-    	if (e.name === 'SequelizeUniqueConstraintError') {
-    		return msg.reply('That tag already exists.');
-      }
-      console.log(e);
-      return msg.reply('Something went wrong with adding a tag.');
-    }
-
-
-  } else if (command === 'tag') {
-
-    // //This section can be removed
-    // // equivalent to: SELECT * FROM tags WHERE name = 'tagName' LIMIT 1;
-    // const tag = await Tags.findOne({ where: { name: args[0] } });
-    // if (tag) {
-    // 	// equivalent to: UPDATE tags SET usage_count = usage_count + 1 WHERE name = 'tagName';
-    // 	tag.increment('usage_count');
-    // 	return msg.channel.send(tag.get('description'));
-    // }
-    // return msg.reply(`Could not find tag: ${tagName}`);
-
-  } else if (command === 'edittag') {
-    // [zeta]
-    const tagName = args[0];
-    const tagDescription = args[1];
-
-    // equivalent to: UPDATE tags (description) values (?) WHERE name='?';
-    const affectedRows = await Tags.update({ description: tagDescription }, { where: { name: tagName } });
-    if (affectedRows > 0) {
-    	return msg.reply(`Tag ${tagName} was edited.`);
-    }
-    return msg.reply(`Could not find a tag with name ${tagName}.`);
-
-  } else if (command === 'taginfo') {
-    
-    const tagName = args[0];
-
-    // equivalent to: SELECT * FROM tags WHERE name = 'tagName' LIMIT 1;
-    const tag = await Tags.findOne({ where: { name: tagName } });
-    if (tag) {
-    	return msg.channel.send(`${tagName} was created by ${tag.username} at ${tag.createdAt} and has been used ${tag.usage_count} times.`);
-    }
-    return msg.reply(`Could not find tag: ${tagName}`);
-    
-  } else if (command === 'showtags') {
-    // equivalent to: SELECT name FROM tags;
-    const tagList = await Tags.findAll({ attributes: ['name'] });
-    const tagString = tagList.map(t => t.name).join(', ') || 'No tags set.';
-    return msg.channel.send(`List of tags: ${tagString}`);
-
-  } else if (command === 'removetag') {
-    const tagName = args[0];
-    // equivalent to: DELETE from tags WHERE name = ?;
-    const rowCount = await Tags.destroy({ where: { name: tagName } });
-    if (!rowCount) return message.reply('That tag did not exist.');
-
-    return msg.reply('Tag deleted.');
-
-  }
-
-});
-
-
 client.on('message', async msg => {
   if (!msg.content.startsWith(PREFIX) || msg.author.bot) return;
   const args = msg.content.slice(PREFIX.length).trim().split(' ');
@@ -206,11 +84,30 @@ client.on('message', async msg => {
     console.log(e);
     return msg.reply(`Could not find tag: ${command}`);
   }
+  
+  if(command === 'args-info'){
+    checkArgs(msg, args);
+    msg.channel.send(`Command name: ${command}\nArguments: ${args}`);
+  }
+  else if(command === 'clearchat'){
+    checkArgs(msg, args);
+    client.commands.get('clearchat').execute(msg, args);
+    //clearChat(msg, args);
+  }
+  else if(command === 'help'){
+    client.commands.get('help').execute(msg, client);
+  }
+  else if(command === 'avatar'){
+    checkArgs(msg, args);
+    client.commands.get('avatar').execute(msg, args);
+  }
+  else if(command === 'tag'){
+    client.commands.get('tag').execute(msg, args, client);
+  }
+  else if(command === 'ping'){
+    client.commands.get('ping').execute(msg, args);
+  }
 });
-
-
-
-
 
 function checkArgs(msg, args){
   if (!args.length) {
@@ -218,7 +115,17 @@ function checkArgs(msg, args){
   }
 }
 
+/**Testing-Playground**/
+//This when user delete message YELL AT THEM!!!!
+// client.on('messageDelete', msg => {
+//     // Create the attachment using MessageAttachment
+//     const attachment = new Discord.MessageAttachment('https://images-ext-2.discordapp.net/external/gSnYcbkGLhtVvwNloUomWx0762Nnfeduwkembi-SBy0/https/media.discordapp.net/attachments/620183192879628298/735538633255419924/unknown.png');
+//     // Send the attachment in the message channel
+//     msg.channel.send(attachment);
+// });
+
 //This function is to let user get role
+
 function modUser(msg, color){
   for(let key in ROLE_COLOR){
     if (color[0] === `${key}`) {
@@ -226,8 +133,6 @@ function modUser(msg, color){
     }
   }
 }
-
-/**Testing-Playground**/
 
 //Login bot
 client.login(process.env.BOT_TOKEN);
--- commands/help.js
+++ commands/help.js
@@ -5,9 +5,18 @@ module.exports = {
     description: 'HELP!',
     execute(msg, client){
         const embed = new Discord.MessageEmbed()
-                        .setAuthor(client.user.username, client.user.displayAvatarURL())              
                         .setColor(0x3ba3ee)
-                        .addField('Murrr cmd', 'murrr\nshock\nnoted\nlc\nsad\nomg\nroll\nnoice\nsadED\n忍')
+                        .setAuthor(client.user.username, client.user.displayAvatarURL())
+                        .setTimestamp()              
+                        .setDescription(
+                            'Need help? Here it is.\n\n' + 
+                            '**Commands**\n\n' +
+                            '**Tags**\n' +
+                            '`~tag add [name] [description]` to **Add** tag \n' + 
+                            '`~tag edit [name] [description]` to **Edit** tag \n' +
+                            '`~tag info [name]` to **Show** tag **info** \n' + 
+                            '`~tag remove [name]` to **Remove** tag \n' + 
+                            '`~tag list` to **show all** tags list \n\n');
         msg.channel.send(embed);
     }
 };
